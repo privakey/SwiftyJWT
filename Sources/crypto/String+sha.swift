7ec11659a596d70cd057cf0e99017611b7fd4178
Joseph Fischetti <jfischetti@probaris.com>
2018-05-30 15:42:27 -0400

    Converted string sha extension to class

--- Sources/crypto/String+sha.swift
+++ Sources/crypto/String+sha.swift
@@ -9,9 +9,9 @@
 import Foundation
 import CCommonCrypto
 
-public extension String {
-    public func sha512() -> String {
-        let data = self.data(using: String.Encoding.utf8)!
+public class StringCryptoUtil {
+    public static func sha512(input: String) -> String {
+        let data = input.data(using: String.Encoding.utf8)!
         var digest = [UInt8](repeating: 0, count:Int(CC_SHA512_DIGEST_LENGTH))
         data.withUnsafeBytes {
             _ = CC_SHA512($0, CC_LONG(data.count), &digest)
